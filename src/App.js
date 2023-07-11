eccb756be732033d49766fae82a29a56b1d7daab
SuprithaReddy12 <127644546+SuprithaReddy12@users.noreply.github.com>
2023-07-11 19:25:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
+import { useState } from 'react';
 import './App.css';
-import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
-import Navbar from './components/navbar';
-import Shop from './pages/shop/shop';
-import Cart from './pages/cart/cart';
-import { AppRoute } from './pay/payment'
-import ShopContextProvider from './context/shop-context';
-import Paid from './pay/paid';
 
 function App() {
+  let [time, setTime] = useState(null)
+  let [interval, setint] = useState(null)
+  const handleTime = (e) => {
+    e.preventDefault();
+    let data = new FormData(e.target)
+    let gettime = data.get("number")
+    setTime(gettime)
+    setint(setInterval(() => {
+      setTime((time) => {
+        if (time > 0) {
+          return time - 1
+        }
+        destroy();
+        return time;
+      })
+    }, 1000));
+  }
+  const destroy = () => {
+    if (interval) {
+      clearInterval(setint)
+      setint(null)
+    }
+  }
   return (
     <div className="App">
-      <ShopContextProvider>
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path='/' element={<Shop/>} />
-          <Route path='/cart' element={<Cart/>} />
-          <Route path='/payment' element={<AppRoute/>} />
-          <Route path='/paid' element={<Paid/>} />
-        </Routes>
-      </Router>
-      </ShopContextProvider>
+      <h1>CountDown Timer</h1>
+      <div id='current-time'>
+        <form onSubmit={(e) => handleTime(e)}>
+          <input
+            type="number"
+            name='number'
+            min="0"
+            placeholder='Enter time in seconds'
+            id='timeCount'>
+          </input>
+          <button type="submit" style={{ marginLeft: "10px" }}>
+            Start
+          </button>
+        </form>
+        <div>{time}</div>
+      </div>
     </div>
   );
 }
